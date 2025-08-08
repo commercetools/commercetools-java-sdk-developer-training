2f9d0a8b0c3e9c2b9281c5dc4d3dbe1a1d92d65b
Nagesh Dixit <nagesh.dixit-ext@commercetools.com>
2025-08-08 10:15:35 -0400

    sync fe with ts

--- src/main/resources/static/tasks/utils.js
+++ src/main/resources/static/tasks/utils.js
@@ -1,37 +1,39 @@
 function handleResponse(response) {
-    if (response.status === 501) {
-        return response.json().then(error => {
-            throw new Error('The function is not implemented on the server.');
-        });
-    }
-    if (response.status === 404) {
-                throw new Error('Not found.');
-        }
-    if (!response.ok) {
-        return response.json().then(error => {
-            throw new Error(error.message);
-        });
-    }
-    return response.json();
+  if (response.status === 501) {
+    return response.json().then((error) => {
+      throw new Error('The function is not implemented on the server.');
+    });
+  }
+  if (response.status === 404) {
+    throw new Error('Not found.');
+  }
+  if (!response.ok) {
+    return response.json().then((error) => {
+      throw new Error(error.message);
+    });
+  }
+  return response.json();
 }
 
-function getStores() {
-    const url = '/api/stores';
-    fetch(url)
-            .then(handleResponse)
-            .then(stores => {
-                if (stores.results && stores.results.length > 0) {
-                    const storesKeyValue = stores.results.reduce((storesInfo, store) => {
-                      storesInfo[store.key] = store.name['en-US'];
-                      return storesInfo;
-                    }, {});
-                    localStorage.setItem('storesList', JSON.stringify(storesKeyValue));
-
-                } else {
-                    localStorage.removeItem('storesList');
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+function getCountries() {
+  const url = '/api/project-settings/countries';
+  return fetch(url)
+    .then(handleResponse)
+    .then((countries) => {
+      countries = countries.sort();
+      console.log('Available countries:', countries);
+      if (countries && countries.length > 0) {
+        localStorage.setItem('countriesList', JSON.stringify(countries));
+      } else {
+        localStorage.removeItem('countriesList');
+      }
+      return countries; // Return the countries for chaining
+    })
+    .catch((error) => {
+      console.error('Error fetching countries:', error);
+      localStorage.removeItem('countriesList'); // Clean up on error
+      throw error; // Re-throw to allow calling code to handle it
+    });
 }
+
+var countries = getCountries();
